01f54337c922517f082d9fefb09e520106dbf2bb
Yogesh Patil <162813010+Yogeshpatil2223@users.noreply.github.com>
2025-10-08 14:58:32 +0530

    Test Case Updated

---
+++ src/evaluate.py
@@ -0,0 +1,11 @@
+import joblib
+import numpy as np
+
+def evaluate_model():
+    model = joblib.load("models/model.pkl")
+    test_x = np.array([[6],[7],[8]])
+    preds = model.predict(test_x)
+    return preds
+
+if __name__ == "_main__":
+    evaluate_model()
